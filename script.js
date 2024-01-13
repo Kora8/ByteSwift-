c5537118c768bc36e83cf6eb15fdb0e29a3719f3
Kora8 <102631320+Kora8@users.noreply.github.com>
2024-01-13 13:14:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+ var botones = document.getElementsByClassName('button');
+ for (var i = 0; i < botones.length; i++) {
+   botones[i].addEventListener('click', function() {
+     window.location.href = 'https://wa.me/524431404409';
+   });
+ }
